5d4f9aedb1d30caeb4efd483ba119313cc605373
Siddharth Prakash <iamsid0011@gmail.com>
2024-06-09 13:34:55 +0530

    today's first commint

---
+++ Interview Questions/doublyLinkedList/REversal/DoublyLinkedList.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public: 
+        int value;
+        Node* next;
+        Node* prev;
+    
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        DoublyLinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+    
+        ~DoublyLinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+    
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty" << endl;
+                return;
+            }
+            while (temp->next != nullptr) {
+                cout << temp->value << " <-> ";
+                temp = temp->next;
+            }
+            cout << temp->value << endl;
+        }
+    
+        Node* getHead() {
+            return head;
+        }
+    
+        Node* getTail() {
+            return tail;
+        }
+    
+        int getLength() {
+            return length;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            length++;
+        }
+        
+        void reverse(){
+            Node* current=head;
+            Node* temp=nullptr;
+            while(current!=nullptr){
+                temp=current->prev;
+                current->prev=current->next;
+                current->next=temp;
+                current=current->prev;
+            }
+            temp=head;
+            head=tail;
+            tail=temp;
+        }
+};
+
+
