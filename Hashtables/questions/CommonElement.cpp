a2e14fcd76b348a27e66d58da141d81c61e80677
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-10 23:26:13 +0530

    commit

--- Hashtables/questions/CommonElement.cpp
+++ Hashtables/questions/CommonElement.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 bool itemInCommon(vector<int> vect1, vector <int> vect2){
     for(auto i : vect1){
@@ -12,8 +13,22 @@ bool itemInCommon(vector<int> vect1, vector <int> vect2){
     return false;
 }
 
+//here the time complexity is o(n^2)
+
+bool itemInCommonHash(vector<int> vect1, vector <int> vect2){
+    unordered_map<int,bool> myMap;
+    for(auto i :vect1){
+        myMap.insert({i,true});
+    }
+    for(auto j: vect2){
+        if(myMap[j]) return true;
+    }
+    return false;
+}
+
+//here the time complexity is o(2n) or o(n)
 int main(){
     vector<int> vect1{1,3,5};
     vector<int> vect2{2,4,5};
-    cout<<itemInCommon(vect1,vect2);
+    cout<<itemInCommonHash(vect1,vect2);
 }
