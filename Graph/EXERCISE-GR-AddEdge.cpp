6fb24a5631177c4ff72f1e84ec27f27575e93572
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-13 23:55:30 +0530

    c

--- Graph/EXERCISE-GR-AddEdge.cpp
+++ Graph/EXERCISE-GR-AddEdge.cpp
@@ -33,12 +33,15 @@ class Graph {
         }
 
         // WRITE ADDEDGE MEMBER FUNCTION HERE //
-        //                                    //
-        //                                    //
-        //                                    //
-        //                                    //
-        //                                    //
-        ////////////////////////////////////////
+        bool addEdge(string vertex1, string vertex2){
+            if(adjList.count(vertex1) != 0 && adjList.count(vertex2) !=0){
+                adjList.at(vertex1).insert(vertex2);
+                adjList.at(vertex2).insert(vertex1);
+                return true;
+            }
+            return false;
+        }
+
 
 };
 
