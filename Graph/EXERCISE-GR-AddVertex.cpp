6ef7697db03fa93e2786bbd0793e4cf79a2d0a4f
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-13 23:43:03 +0530

    add vertex

--- Graph/EXERCISE-GR-AddVertex.cpp
+++ Graph/EXERCISE-GR-AddVertex.cpp
@@ -12,6 +12,7 @@ class Graph {
     public:
         void printGraph() {
             unordered_map<string, unordered_set<string>>::iterator kvPair = adjList.begin();
+            //here the unordered map(hashmap was made) here key is the string the value is an unordered set and we call this map adjList
             while (kvPair != adjList.end()) {
                 cout  << kvPair->first << ": [ ";  // this prints out the vertex
                 unordered_set<string>::iterator edge = kvPair->second.begin();
@@ -23,14 +24,16 @@ class Graph {
                 kvPair++;
             }
         }
+        bool addVertex(string vertex){
+            if(adjList.count(vertex)==0){
+            //this vertex is not already in the adjacency list
+                adjList[vertex];
+                return true;
+            }
+            return false;
+        }
+        // WRITE ADDVERTEX MEMBER FUNCTION 
 
-        // WRITE ADDVERTEX MEMBER FUNCTION HERE //
-        //                                      //
-        //                                      //
-        //                                      //
-        //                                      //
-        //                                      //
-        //////////////////////////////////////////
 
 };
 
