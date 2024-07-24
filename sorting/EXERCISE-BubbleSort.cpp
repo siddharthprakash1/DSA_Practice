b6d5aaba781c4fc8028ed313da2c2c4cf7932c69
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-25 00:24:38 +0530

    c

--- sorting/EXERCISE-BubbleSort.cpp
+++ sorting/EXERCISE-BubbleSort.cpp
@@ -9,6 +9,17 @@ using namespace std;
 	//                                 //
 	//                                 //
 	/////////////////////////////////////
+    void bubbleSort(int array[],int size){
+        for(int i=size-1;i>0;i--){
+            for(int j=0;j<6;j++){
+                if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+    }
 
 
 
