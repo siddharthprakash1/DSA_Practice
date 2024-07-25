c8814c8f63a3b289b816b8760055650967b84b32
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-25 21:40:17 +0530

    c

--- sorting/EXERCISE-InsertionSort.cpp
+++ sorting/EXERCISE-InsertionSort.cpp
@@ -10,7 +10,15 @@ using namespace std;
 	//                                    //
 	////////////////////////////////////////
     void insertionSort(int array[],int size){
-        
+        for(int i=1;i<size;i++){
+            int temp=array[i];
+            int j=i-1;
+            while(j>-1 && temp<array[j]){
+                array[j+1]=array[j];
+                array[j]=temp;
+                j--;
+            }
+        }
     }
 
 
