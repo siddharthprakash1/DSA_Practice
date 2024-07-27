685d0539d14a12421430c208f6f0d0bbb64b5584
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-27 23:23:29 +0530

    c

--- sorting/EXERCISE-Pivot.cpp
+++ sorting/EXERCISE-Pivot.cpp
@@ -16,6 +16,17 @@ void swap(int array[], int firstIndex, int secondIndex) {
 	//                            //
 	//                            //
 	////////////////////////////////
+int pivot(int array[],int pivotIndex, int endIndex){
+    int swapIndex=pivotIndex;
+    for(int i=pivotIndex+1;i<=endIndex;i++){
+        if(array[i]<array[pivotIndex]){
+            swapIndex++;
+            swap(array,swapIndex,i);
+        }
+    }
+    swap(array,pivotIndex,swapIndex);
+    return swapIndex;
+}
 
 
 
