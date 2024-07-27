b5edf94f1c3bc0bb8f294244623b467dc4fd2bd4
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-27 12:34:24 +0530

    c

--- sorting/EXERCISE-Merge.cpp
+++ sorting/EXERCISE-Merge.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<vector>
 using namespace std;
 
 
@@ -9,8 +9,45 @@ using namespace std;
 	//                            //
 	//                            //
 	////////////////////////////////
-    void merge(int array[],int leftIndex,int midIndex,int rightIndex){
-        
+void merge(int array[],int leftIndex,int midIndex,int rightIndex){
+    int leftArraySize=midIndex-leftIndex+1;
+        int rightArraySize=rightIndex-midIndex;
+        int* leftArray = new int[leftArraySize];
+        int* rightArray = new int[rightArraySize];
+
+
+        for (int i=0;i<leftArraySize;i++){
+            leftArray[i]=array[leftIndex+i];
+        }
+        for (int j=0;j<rightArraySize;j++){
+            rightArray[j]=array[midIndex+1+j];
+        }
+        int index=leftIndex;
+        int i=0;
+        int j=0;
+
+        while(i<leftArraySize && j<rightArraySize){
+            if(leftArray[i]<=rightArray[j]){
+                array[index]=leftArray[i];
+                index++;
+                i++;
+            }else{
+                array[index]=rightArray[j];
+                index++;
+                j++;
+            }
+        }
+        while(i<leftArraySize){
+            array[index]=leftArray[i];
+            index++;
+            i++;
+            
+        }
+        while(j<rightArray[j]){
+            array[index]=rightArray[j];
+            index++;
+            j++;
+        }
     }
  
 int main() {
