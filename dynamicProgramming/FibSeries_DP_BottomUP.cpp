dc8866be3f8d9fdaa83efad889f45820913b4489
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-28 12:52:37 +0530

    c

---
+++ dynamicProgramming/FibSeries_DP_BottomUP.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+static int counter = 0;
+
+int fib(int n) {
+    vector<int> fiblist;
+    fiblist.push_back(0);
+    fiblist.push_back(1);
+    for(int index = 2; index <= n; index++) {
+        counter++;
+        fiblist.push_back(fiblist[index-1] + fiblist[index-2]);
+    }
+    return fiblist[n];
+}
+
+int main() {
+    int n = 7;
+    cout << "\nFib of " << n << " = " << fib(n);
+    cout << "\n\nCounter: " << counter << endl;
+    return 0;
+}
